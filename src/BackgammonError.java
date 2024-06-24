64bfaf09336ab20cb4a122bd04c55dc990a5388b
Jurij Waite <jurijlwaite@gmail.com>
2024-06-24 15:44:54 +0700

    Added an error for not yet implemented methods / features. Documentation.

--- src/BackgammonError.java
+++ src/BackgammonError.java
@@ -1,5 +1,11 @@
 public class BackgammonError extends Error {
+    /** A custom error constructor for errors relating to the Backgammon game. */
     BackgammonError(String errorMessage) {
         super(errorMessage);
+    }
+
+    /** Return a BackgammonError for not yet implemented methods or features. **/
+    public static BackgammonError notImplemented() {
+        return new BackgammonError("This method or feature has yet been implemented.");
     }
 }
