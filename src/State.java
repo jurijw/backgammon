045e517189d08a636ee98f7902f50bb860a1b257
Jurij Waite <jurijlwaite@gmail.com>
2024-06-28 18:39:49 +0700

    Reworked the logic for computing legal moves. Formatting.

--- src/State.java
+++ src/State.java
@@ -1,9 +1,12 @@
-import java.util.ArrayList;
+import java.util.*;
 
-/** This class handles all things related to the STATE of a game. But also provides methods to calculate valid moves. */
-public class Board {
+/**
+ * This class handles all things related to the STATE of a game. But also provides methods to
+ * calculate valid moves.
+ */
+public class State {
 
-    Board() {
+    State() {
         _dice = new Dice();
         _positions = new Positions();
     }
@@ -81,20 +84,27 @@ public class Board {
         int targetIndex = move.target();
 
         if (!occupiedByActivePlayer(startIndex)) {
-            throw new BackgammonError("INVALID CAPTURE ATTEMPT: Attempted to move a piece that does not belong to the" +
-                    " active player.");
+            throw new BackgammonError(
+                    "INVALID CAPTURE ATTEMPT: Attempted to move a piece that does not belong to the"
+                            + " active player.");
         }
         if (_positions.empty(startIndex)) {
-            throw new BackgammonError("INVALID CAPTURE ATTEMPT: Attempted to move a piece from a position with no pieces.");
+            throw new BackgammonError(
+                    "INVALID CAPTURE ATTEMPT: Attempted to move a piece from a position with no "
+                            + "pieces.");
         }
         if (_positions.full(targetIndex)) {
-            throw new BackgammonError("INVALID CAPTURE ATTEMPT: Attempted to move a piece to a full position.");
+            throw new BackgammonError(
+                    "INVALID CAPTURE ATTEMPT: Attempted to move a piece to a full position.");
         }
 
         if (oppositeColorsAtIndices(startIndex, targetIndex)) {
-            // This kind of move is only valid if it is a capturing move. I.e, the target position only has one piece on it. */
+            // This kind of move is only valid if it is a capturing move. I.e, the target
+            // position only has one piece on it. */
             if (!_positions.single(targetIndex)) {
-                throw new BackgammonError("INVALID CAPTURE ATTEMPT: Attempting to move a piece to an opponent's position with more than one piece on it.");
+                throw new BackgammonError(
+                        "INVALID CAPTURE ATTEMPT: Attempting to move a piece to an opponent's "
+                                + "position with more than one piece on it.");
             }
         }
 
@@ -105,15 +115,19 @@ public class Board {
 
 
     /**
-     * Returns true iff the pieces at INDEX1 and INDEX2 are of opposite color. Indices should refer positions ON THE
-     * BOARD.
+     * Returns true iff the pieces at INDEX1 and INDEX2 are of opposite color. Indices should refer
+     * positions ON THE BOARD.
      */
     public boolean oppositeColorsAtIndices(int index1, int index2) {
         _positions.checkValidBoardIndex(index1, index2);
         return (_positions.get(index1) ^ _positions.get(index2)) < 0;
     }
 
-    /** Get the number of pieces at the board position INDEX. Negative numbers indicate black pieces. */
+    // TODO: Consider implementing this everywhere instead of _positions.get()
+    /**
+     * Get the number of pieces at the board position INDEX. Negative numbers indicate black
+     * pieces.
+     */
     public int getNumberPiecesAt(int index) {
         return _positions.get(index);
     }
@@ -123,13 +137,9 @@ public class Board {
         return _positions.occupiedBy(white(), index);
     }
 
-    /** Returns true iff all the active player's pieces (on the board) are in the end zone. */
-    public boolean allPiecesInEndZone() {
-        return allPiecesInEndZone(white());
-    }
-
     /**
-     * Return the number of white pieces remaining on the board if WHITE, else number of black pieces.
+     * Return the number of white pieces remaining on the board if WHITE, else number of black
+     * pieces.
      */
     public int numPiecesRemainingOnBoard(boolean white) {
         int count = 0;
@@ -138,13 +148,15 @@ public class Board {
         }
         return count;
     }
+
     /** Return the number of pieces remaining on the board for the active player. */
     public int numPiecesRemaining() {
         return numPiecesRemainingOnBoard(white());
     }
 
     /**
-     * Returns true iff the player (designated by WHITE) has no pieces behind the position INDEX on the board.
+     * Returns true iff the player (designated by WHITE) has no pieces behind the position INDEX on
+     * the board.
      **/
     public boolean isLastPieceOnBoard(int index, boolean white) {
         for (int position : _positions.occupiedBoardPositions(white)) {
@@ -156,8 +168,8 @@ public class Board {
     }
 
     /**
-     * Returns true iff all of a player's pieces are in the end zone (final 6 positions), or have already "escaped" the
-     * board. The player that is checked for is given by the WHITE boolean.
+     * Returns true iff all of a player's pieces are in the end zone (final 6 positions), or have
+     * already "escaped" the board. The player that is checked for is given by the WHITE boolean.
      */
     public boolean allPiecesInEndZone(boolean white) {
         for (int position : _positions.occupiedBoardPositions(white)) {
@@ -168,12 +180,18 @@ public class Board {
         return true;
     }
 
+    /** Returns true iff all the active player's pieces (on the board) are in the end zone. */
+    public boolean allPiecesInEndZone() {
+        return allPiecesInEndZone(white());
+    }
+
     /** Returns true iff it is white's turn to play. */
     public boolean white() {
         return _white;
     }
 
-    /** Set the active player to WHITE, where if WHITE is true, then it is the white player's turn,
+    /**
+     * Set the active player to WHITE, where if WHITE is true, then it is the white player's turn,
      * otherwise it is black's turn.
      */
     public void setTurn(boolean white) {
@@ -216,8 +234,11 @@ public class Board {
         return isLastPieceOnBoard(index, white());
     }
 
-    /** Returns an integer array containing the indices of all occupied board positions of the active player. */
-    private ArrayList<Integer> activePlayerBoardPositions() {
+    /**
+     * Returns an integer array containing the indices of all occupied board positions of the active
+     * player.
+     */
+    private List<Integer> activePlayerBoardPositions() {
         return _positions.occupiedBoardPositions(white());
     }
 
@@ -226,9 +247,10 @@ public class Board {
         return _positions.hasCapturedPiece(white());
     }
 
-    /** Returns true iff the INDEX provided can be moved to by the player specified by WHITE. That is, the position
-     * is empty, contains only one of the opponent's pieces (indicating it can be captured), or the position is not
-     * fully occupied by pieces of the specified player.
+    /**
+     * Returns true iff the INDEX provided can be moved to by the player specified by WHITE. That
+     * is, the position is empty, contains only one of the opponent's pieces (indicating it can be
+     * captured), or the position is not fully occupied by pieces of the specified player.
      */
     public boolean positionCanBeMovedToBy(int index, boolean white) {
         if (_positions.full(index)) {
@@ -238,45 +260,77 @@ public class Board {
         return white ? numPiecesAtPos >= -1 : numPiecesAtPos <= 1;
     }
 
-    /** Returns true iff the board position at INDEX can be moved to by the active player.
-     *  That is, it is either empty, or contains only one of the opponents pieces.
+    /**
+     * Returns true iff the board position at INDEX can be moved to by the active player. That is,
+     * it is either empty, or contains only one of the opponents pieces.
      */
     private boolean positionCanBeMovedToByActivePlayer(int index) {
         return positionCanBeMovedToBy(index, white());
     }
 
+    /**
+     * Returns true iff an index constitutes a "perfect escape" for the active player. That is,
+     * it overshoots the edge of the board by EXACTLY one position. This is relevant because when
+     * all pieces of one color are in the end zone, any roll that would take a piece exactly one
+     * position of the board is permitted, even if that piece is not the last piece. For example,
+     * if there are black pieces at positions.
+     * @param index
+     * @return
+     */
+    private boolean perfectEscape(int index) {
+        return white() ? index == Positions.BOARD_SIZE : index == -1;
+    }
+
     /** Takes a single roll (1-6) and determines legal moves based on that roll. */
-    public ArrayList<Move> legalMovesFromRoll(int roll) {
-        ArrayList<Move> validMoves = new ArrayList<>();
+    public Set<Move> legalMovesFromRoll(int roll) {
+        Set<Move> validMoves = new HashSet<>(); // TODO: Could maybe make this an instance
+        // variable.
+        roll = white() ? roll : -roll; // This allows black rolls to be counted as negative.
         if (activePlayerHasBeenCaptured()) {
             /* Only permit moves that free the captured piece(s). */
-            int targetIndex = white() ? Positions.BOARD_SIZE - roll : roll - 1;
+            int targetIndex = white() ? roll - 1 : Positions.BOARD_SIZE + roll;
             if (positionCanBeMovedToByActivePlayer(targetIndex)) {
-                // Add the move
+                // TODO: Could consider checking here if the move is a capture and if so add that
+                //  information as a property of the Move instance. Maybe, it's simpler to just
+                //  do this when the actual move is played out...
+                validMoves.add(Move.fromCaptured(white(), targetIndex));
             }
+            return validMoves;
         }
-        roll = white() ? roll : -roll; // This allows black rolls to be counted as negative.
         for (int startIndex : activePlayerBoardPositions()) {
             int targetIndex = startIndex + roll;
-            if (allPiecesInEndZone()) {
-                if (isLastPieceOnBoard(startIndex)) {
-                    // Allow any move that overshoots the board (i.e takes it off the board)
-                } else {
-                    // Allow moves that perfectly take a piece to the end zone. (e.g. there is a piece at position 4 and 5
-                    // and white rolls a 4. In this scenario, the piece at position 4 can escape to the end zone)
+            // Check if the target index takes a piece off the board.
+            if (!Positions.validBoardIndex(targetIndex)) {
+                // Allow if all pieces in end zone AND it is a perfect escape or last piece on
+                // the board
+
+                // Allow any move that overshoots the board (i.e takes it off the board)
+
+                // Allow moves that perfectly take a piece to the end zone. (e.g. there is a
+                // piece at position 4 and 5
+                // and white rolls a 4. In this scenario, the piece at position 4 can escape
+                // to the end zone OR the position 5 piece can be moved to position 1.
+                if (allPiecesInEndZone()) {
+                    if (perfectEscape(targetIndex) || isLastPieceOnBoard(startIndex)) {
+                        validMoves.add(Move.escape(white(), startIndex));
+                    }
+                }
+            } else {
+                if (positionCanBeMovedToBy(targetIndex, white())) {
+                    validMoves.add(Move.move(startIndex, targetIndex));
                 }
             }
-            if (positionCanBeMovedToBy(targetIndex, white())) {
-                // Add move.
-            }
         }
-
         return validMoves;
     }
 
     /** Return an array of all legal moves which can be made by using either roll first. */
-    public ArrayList<Move> legalMoves() {
-        ArrayList<Move> moves = legalMovesFromRoll(first());
+    public Map<Integer, Set<Move>> legalMoves() {
+        Map<Integer, Set<Move>> moves= new HashMap<>();
+        Set<Move> movesFromFirstRoll = legalMovesFromRoll(first());
+        if (!pasch()) {
+
+        }
         moves.addAll(legalMovesFromRoll(second()));
         return moves;
     }
