a285b4b45206735c241b84ff0a6524bf3d55e83a
Jurij Waite <jurijlwaite@gmail.com>
2024-07-05 17:21:36 +0700

    Added methods to check which side a side instance is, as well as get the opponent.

--- src/Side.java
+++ src/Side.java
@@ -3,18 +3,9 @@
  * the game winner as long as the game is not over yet. */
 public enum Side {
     UNDETERMINED(-1, -1),
-    WHITE(Positions.WHITE_ESCAPE_INDEX, Positions.WHITE_CAPTURED_INDEX) {
-        @Override
-        Side opponent() {
-            return BLACK;
-        }
-    },
-    BLACK(Positions.BLACK_ESCAPE_INDEX, Positions.BLACK_CAPTURED_INDEX) {
-        @Override
-        Side opponent() {
-            return WHITE;
-        }
-    };
+    WHITE(Positions.WHITE_ESCAPE_INDEX, Positions.WHITE_CAPTURED_INDEX), // TODO: This will
+        // cause a circular dependency since the Positions class imports the Side enum.
+    BLACK(Positions.BLACK_ESCAPE_INDEX, Positions.BLACK_CAPTURED_INDEX);
 
     Side(int escapeIndex, int captureIndex) {
         this._escapeIndex = escapeIndex;
@@ -23,20 +14,47 @@ public enum Side {
 
     /** Return the opponent of my side. Does not work if the side is undetermined. */
     Side opponent() {
-        throw new BackgammonError("Cannot call opponent if the side is undetermined.");
+        if (this == UNDETERMINED) {
+            throw new BackgammonError("Cannot call opponent if the side is UNDETERMINED.");
+        } else if (this == WHITE) {
+            return BLACK;
+        } else {
+            return WHITE;
+        }
+    }
+
+    /** Throws an exception if side is undetermined. Otherwise, true iff white. */
+    boolean isWhite() {
+        if (this == UNDETERMINED) {
+            throw new BackgammonError("Cannot call isWhite if the side is UNDETERMINED.");
+        }
+        return this == WHITE;
+    }
+
+    /** Throws an exception if side is undetermined. Otherwise, true iff black. */
+    boolean isBlack() {
+        if (this == UNDETERMINED) {
+            throw new BackgammonError("Cannot call isBlack if the side is UNDETERMINED.");
+        }
+        return this == BLACK;
+    }
+
+    /** True iff this side is UNDETERMINED. */
+    boolean isUndetermined() {
+        return this == UNDETERMINED;
     }
 
     /** Return the escape index in the _positions array of a Positions instance for my side. */
     int getEscapeIndex() {
         if (this == UNDETERMINED) {
-            throw new BackgammonError("Can't get escape index for undetermined side.");
+            throw new BackgammonError("Can't get escape index for UNDETERMINED side.");
         }
         return this._escapeIndex;
     }
     /** Return the capture index in the _positions array of a Positions instance for my side. */
     int getCaptureIndex() {
         if (this == UNDETERMINED) {
-            throw new BackgammonError("Can't get capture index for undetermined side.");
+            throw new BackgammonError("Can't get capture index for UNDETERMINED side.");
         }
         return this._captureIndex;
     }
@@ -48,8 +66,7 @@ public enum Side {
     }
 
     /** The escape index for my side in the _positions array of a Positions instance. */
-    public final int _escapeIndex;
+    private final int _escapeIndex;
     /** The capture index for my side in the _positions array of a Positions instance. */
-    public final int _captureIndex;
-
+    private final int _captureIndex;
 }
