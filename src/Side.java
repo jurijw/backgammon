3c2961003548206055670c49ebbb36725f666825
Jurij Waite <jurijlwaite@gmail.com>
2024-07-06 14:27:15 +0700

    The Side enum no longer contains references to position and escape indices. How this is implemented is a detail of the Board (formerly Positions) class.

--- src/Side.java
+++ src/Side.java
@@ -2,15 +2,9 @@
  * be used before the game commences (When the starting side has not yet been determined) and for
  * the game winner as long as the game is not over yet. */
 public enum Side {
-    UNDETERMINED(-1, -1),
-    WHITE(Positions.WHITE_ESCAPE_INDEX, Positions.WHITE_CAPTURED_INDEX), // TODO: This will
-        // cause a circular dependency since the Positions class imports the Side enum.
-    BLACK(Positions.BLACK_ESCAPE_INDEX, Positions.BLACK_CAPTURED_INDEX);
-
-    Side(int escapeIndex, int captureIndex) {
-        this._escapeIndex = escapeIndex;
-        this._captureIndex = captureIndex;
-    }
+    WHITE,
+    BLACK,
+    UNDETERMINED;
 
     /** Return the opponent of my side. Does not work if the side is undetermined. */
     Side opponent() {
@@ -44,29 +38,9 @@ public enum Side {
         return this == UNDETERMINED;
     }
 
-    /** Return the escape index in the _positions array of a Positions instance for my side. */
-    int getEscapeIndex() {
-        if (this == UNDETERMINED) {
-            throw new BackgammonError("Can't get escape index for UNDETERMINED side.");
-        }
-        return this._escapeIndex;
-    }
-    /** Return the capture index in the _positions array of a Positions instance for my side. */
-    int getCaptureIndex() {
-        if (this == UNDETERMINED) {
-            throw new BackgammonError("Can't get capture index for UNDETERMINED side.");
-        }
-        return this._captureIndex;
-    }
-
     /** Return a readable representation of my side. */
     @Override
     public String toString() {
         return this.name().toLowerCase();
     }
-
-    /** The escape index for my side in the _positions array of a Positions instance. */
-    private final int _escapeIndex;
-    /** The capture index for my side in the _positions array of a Positions instance. */
-    private final int _captureIndex;
 }
