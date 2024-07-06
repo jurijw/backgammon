8d77f17c7b5b410f68dab707e954b2744a4de30f
Jurij Waite <jurijlwaite@gmail.com>
2024-07-06 14:31:15 +0700

    Index class renamed to BoardIndex. This class now exclusively refers to indices ON THE BOARD. How captured and escaped pieces are handled is a separate matter and an implementation detail of the Board class.

--- src/BoardIndex.java
+++ src/BoardIndex.java
@@ -1,84 +1,26 @@
-public class Index {
+public class BoardIndex {
     // TODO: Consider splitting indices into board indices and non-board indices. Maybe we don't
     //  even need off-board indices? Perhaps it is better to access escaped and captured pieces
     //  directly through the Positions class?
-    private Index(int index) {
-        this._index = index;
-        this._isBoardIndex = validBoardIndex(index);
+    private BoardIndex(int boardIndex) {
+        this._boardIndex = boardIndex;
     }
 
     /** A getter for my index. */
     public int getIndex() {
-        return _index;
-    }
-
-    /** Return true iff I represent a board index. */
-    public boolean isBoardIndex() {
-        return _isBoardIndex;
+        return _boardIndex;
     }
 
     /** A factory method to return an index instance associated with a position on the board. */
-    public static Index boardIndex(int boardIndex) {
-        checkValidIndex(boardIndex);
+    public static BoardIndex boardIndex(int boardIndex) {
+        ensureValidBoardIndices(boardIndex);
         return INDICES[boardIndex];
     }
 
-    /** Return the escape index associated with SIDE. Throws an error if SIDE is UNDETERMINED. */
-    public static Index escapeIndex(Side side) {
-        if (side.isUndetermined()) {
-            throw new BackgammonError("Cannot get escape index for UNDETERMINED side.");
-        }
-        if (side.isWhite()) {
-            return INDICES[Structure.WHITE_ESCAPE_INDEX];
-        }
-        return INDICES[Structure.BLACK_ESCAPE_INDEX];
-    }
-
-    /** Return the capture index associated with SIDE. Throws an error if SIDE is UNDETERMINED." */
-    public static Index captureIndex(Side side) {
-        if (side.isUndetermined()) {
-            throw new BackgammonError("Cannot get escape index for UNDETERMINED side.");
-        }
-        if (side.isWhite()) {
-            return INDICES[Structure.WHITE_CAPTURED_INDEX];
-        }
-        return INDICES[Structure.BLACK_CAPTURED_INDEX];
-    }
-
-    /** Return the escape index associated with the white SIDE */
-    public static Index whiteEscapeIndex() {
-        return INDICES[Structure.WHITE_ESCAPE_INDEX];
-    }
-
-    /** Return the escape index associated with the black SIDE */
-    public static Index blackEscapeIndex() {
-        return INDICES[Structure.BLACK_ESCAPE_INDEX];
-    }
-
     /**
-     * Throws an invalid position index error, with an appended MESSAGE for additional information.
+     * Returns true iff all INDICES refers to a valid board index.
      */
-    private static void throwInvalidPositionIndexError(String message) {
-        throw new BackgammonError("INVALID POSITION INDEX: " + message);
-    }
-
-    /**
-     * Returns true iff INDEX refers to a valid index in the _positions array.
-     */
-    private static boolean validIndex(int... indices) {
-        for (int index : indices) {
-            if ((index < 0) || (index >= Structure.SIZE)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    /**
-     * Returns true iff INDEX refers to a valid board index. That is, not to an index that stores
-     * escaped or captured pieces.
-     */
-    private static boolean validBoardIndex(int... indices) {
+    private static boolean validBoardIndices(int... indices) {
         for (int index : indices) {
             if ((index < 0) || (index >= Structure.BOARD_SIZE)) {
                 return false;
@@ -87,65 +29,21 @@ public class Index {
         return true;
     }
 
-    /**
-     * Throws an error if the passed INDEX is not valid. Appends MESSAGE to the error message
-     */
-    private static void checkValidIndex(String message, int... indices) {
-        if (!validIndex(indices)) {
-            throwInvalidPositionIndexError(message);
-        }
-    }
-
-    /**
-     * Throw an error if the passed INDEX is not valid without providing additional error
-     * information.
-     */
-    private static void checkValidIndex(int... indices) {
-        checkValidIndex("", indices);
-    }
-
-    /**
-     * Throws an error if INDEX is not a valid board index. MESSAGE is used to pass additional error
-     * information.
-     */
-    private void checkValidBoardIndex(String message, int... indices) {
-        if (!validBoardIndex(indices)) {
-            throw new BackgammonError("INVALID BOARD INDEX: " + message);
-        }
-    }
-
-    /**
-     * Throws an error if INDEX is not a valid board index without adding additional error
-     * information.
-     */
-    private void checkValidBoardIndex(int... indices) {
-        checkValidBoardIndex("", indices);
-    }
-
-    /** Throw an error if my index does not refer to a board index. */
-    void ensureValidBoardIndex() {
-        if (!isBoardIndex()) {
-            throw new BackgammonError("Index does not correspond to a board index.");
-        }
-    }
-
-    /** Throws an error if any of the passed INDICES are not board indices. */
-    static void ensureValidBaordIndices(Index... indices) {
-        for (Index index : indices) {
-            index.ensureValidBoardIndex();
+    /** Throws an error if the given INDEX is not a valid board index. */
+    private static void ensureValidBoardIndices(int... indices) {
+        if (!validBoardIndices(indices)) {
+            throw new BackgammonError("One or more of the passed indices are not valid board "
+                                              + "indexes.");
         }
     }
 
     /** The index in the _positions array I represent. */
-    private final int _index;
+    private final int _boardIndex;
 
-    /** True iff I represent an index on the board (i.e. not a capture / escape index). */
-    private final boolean _isBoardIndex;
-
-    private static final Index[] INDICES = new Index[Structure.SIZE];
+    private static final BoardIndex[] INDICES = new BoardIndex[Structure.SIZE];
     static {
         for (int i = 0; i < Structure.SIZE; i++) {
-            INDICES[i] = new Index(i);
+            INDICES[i] = new BoardIndex(i);
         }
     }
 }
