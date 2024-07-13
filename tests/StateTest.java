4813cc5d078fd00d5301e665353e11a5cbcf1dab
Jurij Waite <jurijlwaite@gmail.com>
2024-07-13 16:29:30 +0700

    Added first proper integration test for black pieces escaping the board.

--- tests/StateTest.java
+++ tests/StateTest.java
@@ -2,30 +2,64 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StateTest {
-    static State defaultState;
+
+    static final Map<String, Object> blackEscapeConfig = Map.of(
+        "extendedSetup", TestSetups.ESCAPE,
+        "first", 1,
+        "second", 2,
+        "currentSide", Side.BLACK,
+        "remainingRolls", new int[] { 1, 2 }
+    );
+
+    /** A convenient shorthand for creating BoardIndex objects. */
+    static BoardIndex bi(int i) {
+        return BoardIndex.make(i);
+    }
 
     @BeforeEach
     void setUp() {
-        defaultState = new State();
+    }
+
+    @Test
+    void fromMap() {
+        State s = State.fromMap(blackEscapeConfig);
+        assertEquals(blackEscapeConfig.get("currentSide"), s.getCurrentSide());
+        assertEquals(blackEscapeConfig.get("first"), s.first());
+        assertEquals(blackEscapeConfig.get("second"), s.second());
+        for (int i = 0; i < s.getRemainingRolls().size(); i++) {
+            assertEquals(blackEscapeConfig.get("remainingRolls"), s.getRemainingRolls().get(i),
+                         "Failed at index: " + i);
+        }
+        // Check the board is properly setup.
     }
 
     @Test
     void testBlackEscape() {
-//        State s = new State()
-//        State s = new State(false, 1, 2, TestSetups.ESCAPE);
-//        Set<Move> expectedLegalMoves = new HashSet<>();
-//        expectedLegalMoves.add(Move.boardMove(0, 25, 1));
-//        expectedLegalMoves.add(Move.boardMove(0, 25, 2));
-//        assertEquals(expectedLegalMoves, s.getLegalMoves());
-//
-//        /* Play an escaping move. */
-//        s.makeMove(Move.boardMove(0, 25, 1));
-        
+        State s = State.fromMap(blackEscapeConfig);
+        int numEscapedBlackInitial = s.getBoard().numEscaped(Side.BLACK);
+        int numEscapedWhiteInitial = s.getBoard().numEscaped(Side.WHITE);
+        Set<Move> expectedLegalMoves = new HashSet<>();
+        expectedLegalMoves.add(EscapeMove.move(bi(0), 1, Side.BLACK));
+        expectedLegalMoves.add(EscapeMove.move(bi(0), 2, Side.BLACK));
+        assertEquals(expectedLegalMoves, s.getLegalMoves());
+        /* Play an escaping move. */
+        s.makeMove(EscapeMove.move(bi(0), 1, Side.BLACK));
+        assertEquals(List.of(2), s.getRemainingRolls());
+        expectedLegalMoves.remove(EscapeMove.move(bi(0), 1, Side.BLACK));
+        assertEquals(expectedLegalMoves, s.getLegalMoves());
+        assertEquals(Side.BLACK, s.getCurrentSide());
+        assertEquals(numEscapedBlackInitial + 1, s.getBoard().numEscaped(Side.BLACK));
+        assertEquals(numEscapedWhiteInitial, s.getBoard().numEscaped(Side.WHITE));
+        /* Play the only remaining escape move. */
+        s.makeMove(EscapeMove.move(bi(0), 2, Side.BLACK));
+        assertEquals(Side.WHITE, s.getCurrentSide());
     }
 
     @Test
@@ -61,7 +95,7 @@ class StateTest {
     }
 
     @Test
-    void getAvailableRolls() {
+    void getRemainingRolls() {
     }
 
     @Test
